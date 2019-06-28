2f78087b30bfcfcb1c3c2e4d7a22cb9dfb7c7fd6
bominn <r07942091@ntu.edu.tw>
2019-06-28 23:56:14 +0800

    add retinanet script

---
+++ final/retinanet_test.sh
@@ -0,0 +1,3 @@
+python3 ./src/preprocessing.py
+python3 ./src/retinanet_test.py
+python3 ./src/write.py
---
+++ final/retinanet_train.sh
@@ -0,0 +1,2 @@
+python3 ./src/preprocessing.py
+python3 ./src/retinanet_train.py --dataset csv --csv_train train.csv --csv_classes label.csv --csv_val valid.csv
